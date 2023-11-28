7d464d0da9017e34dd63a0d98f15b5d20b9b4333
يوسف محمد احمد ابراهيم <132560520+yousef266@users.noreply.github.com>
2023-11-28 18:30:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,81 +1,86 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.css"
 import React, { useState } from "react";
 
 const GpaCalculator = () => {
   const [totalCredits, setTotalCredits] = useState(0);
   const [totalGradePoints, setTotalGradePoints] = useState(0);
-  const [count, setcount] = useState(0);
-  const [credit, setCredit] = useState("");
-  const [grade, setGrade] = useState("");
+  const [grades, setGrades] = useState({});
   const [errorMessage, setErrorMessage] = useState("");
+  // const [lastTotalGrade, setlastTotalGradePoints]=useState(0);
+  const [totalGpa, setTotalGpa]=useState(0);
+
+  const user = {
+    totalHours: 37,
+    totalGpa: 3.8,
+  };
+
+  const data = [
+    { id: 1, name: "sa:", hours: 3 },
+    { id: 2, name: "dc:", hours: 3 },
+    { id: 3, name: "ds:", hours: 3 },
+    { id: 4, name: "pl2:", hours: 3 },
+    { id: 5, name: "logic:", hours: 3 },
+    { id: 6, name: "db:", hours: 3 },
+    { id: 7, name: "hr:", hours: 2 },
+  ];
 
   const calculateGPA = () => {
-    const parsedCredit = parseInt(credit);
-    const uppercaseGrade = grade.toUpperCase();
-    setcount(count + 1)
+    let totalCreditsValue = 0;
+    let totalGradePointsValue = 0;
 
-    if (
-      isNaN(parsedCredit) ||
-      parsedCredit <= 0 ||
-      !isValidGrade(uppercaseGrade)
-    ) {
-      setErrorMessage("Please enter valid credit hours and grade.");
-      setcount(count)
-      return;
+    for (const subject of data) {
+      const grade = grades[subject.id];
+
+      if (!grade || !isValidGrade(grade)) {
+        setErrorMessage("Please enter valid grades for all subjects.");
+        return;
+      }
+
+      const gradePoints = calculateGradePoints(grade);
+      const coursePoints = subject.hours * gradePoints;
+
+      totalCreditsValue += subject.hours;
+      totalGradePointsValue += coursePoints;
     }
-    if (count >= 7) {
-      setErrorMessage(
-        "The number of subjects is more than the permitted number"
-      );
-      setcount(count)
 
-      return;
-    }
-    if (credit < 2) {
-      setErrorMessage(
-        "Min hour is 2."
-      );
-      setcount(count)
-      return ;
-    } else if (credit > 3) {
-      setErrorMessage(
-        "Max hour is 3."
-      );
-      setcount(count)
-      return ;
-    }
-    const gradePoints = calculateGradePoints(uppercaseGrade);
-    const coursePoints = parsedCredit * gradePoints;
+    let lastTotalGrade = user.totalGpa * user.totalHours;
+    console.log(lastTotalGrade);
+    let totalGrade = lastTotalGrade + totalGradePointsValue;
+    console.log(totalGrade);
+    setTotalCredits(totalCreditsValue + user.totalHours);
+    console.log(totalCreditsValue);
+    setTotalGradePoints(totalGradePointsValue+totalGrade);
+    setTotalGpa((totalGrade / (totalCreditsValue+user.totalHours)).toFixed(2))
 
-    setTotalCredits(totalCredits + parsedCredit);
-    setTotalGradePoints(totalGradePoints + coursePoints);
-
-    const gpa = (totalGradePoints / totalCredits).toFixed(2);
 
     setErrorMessage("");
-
-    
-    setCredit("");
-    setGrade("");
   };
+
   const isValidGrade = (grade) => {
-    const validGrades = ["A", "B", "C", "D", "F"];
-    return validGrades.includes(grade);
+    const validGrades = ["A", "A+", "B", "B+", "C", "C+", "D", "D+", "F"];
+    return validGrades.includes(grade.toUpperCase());
   };
 
   const calculateGradePoints = (grade) => {
-    switch (grade) {
-      case "A":
+    switch (grade.toUpperCase()) {
+      case "A+":
         return 4.0;
+      case "A":
+        return 3.75;
+      case "B+":
+        return 3.4;
       case "B":
-        return 3.0;
+        return 3.1;
+      case "C+":
+        return 2.8;
       case "C":
-        return 2.0;
+        return 2.5;
+      case "D+":
+        return 2.25;
       case "D":
-        return 1.0;
+        return 2.0;
       case "F":
-        return 0.0;
+        return 1.0;
       default:
         return 0.0;
     }
@@ -83,43 +88,36 @@ const GpaCalculator = () => {
 
   return (
     <div>
-      <h1 className="header">GPA Calculate </h1>
-      <div>
-        <div className="hour">
-          <label htmlFor="credit">Credit Hours:</label>
-          <input
-            min={2}
-            // type="number"
-            id="credit"
-            value={credit}
-            onChange={(e) => setCredit(e.target.value)}
-          />
-        </div>
-        <div className="grade">
-          <label htmlFor="grade" className="grade">
-            Grade:
-          </label>
-          <input 
-            placeholder="Vlaid Input ( A, B, C, D, F)"
-            className="grade-input"
-            type="text"
-            id="grade"
-            value={grade}
-            onChange={(e) => setGrade(e.target.value)}
-          />
-        </div>
+      <h1 className="header">GPA Calculator</h1>
+      <div className="grade">
+        {data.map((subject) => (
+          <div key={subject.id} className="margin">
+            <div className="namesub">{subject.name}</div>
+            <input
+              placeholder="Valid Input (A, B, C, D, F)"
+              className="grade-input"
+              type="text"
+              id={`grade-${subject.id}`}
+              value={grades[subject.id] || ""}
+              onChange={(e) => {
+                setGrades((prevGrades) => ({
+                  ...prevGrades,
+                  [subject.id]: e.target.value.toUpperCase(),
+                }));
+              }}
+            />
+          </div>
+        ))}
       </div>
-      {/* <div>{count}</div> */}
       <button onClick={calculateGPA}>Calculate GPA</button>
       {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       <div id="result">
-        <p>number of subjects: {count}</p>
         <p>Total Credits: {totalCredits}</p>
         <p>
           GPA:{" "}
           {isNaN(totalCredits)
             ? "N/A"
-            : (totalGradePoints / totalCredits).toFixed(2)}
+            : totalGpa}
         </p>
       </div>
     </div>
